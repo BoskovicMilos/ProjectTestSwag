b37cad1938ddbf8f48f0f08c24f8a96fdfb21d2d
Milos Boskovic <milos.c.boskovic@gmailcom>
2023-07-26 23:10:05 +0200

    AddSecondTest

--- src/test/java/SwaglabTest.java
+++ src/test/java/SwaglabTest.java
@@ -5,6 +5,11 @@ import org.testng.annotations.Test;
 
 public class SwaglabTest extends BaseTest{
     LoginPage loginPage;
+    InventoryPage inventoryPage;
+    CartPage cartPage;
+
+    CheckoutStepOnePage checkoutStepOnePage;
+
 
     @BeforeMethod
 
@@ -12,6 +17,11 @@ public class SwaglabTest extends BaseTest{
     {
         driver = openBrowser();
         loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutStepOnePage= new CheckoutStepOnePage(driver);
+
+
 
     }
 
@@ -19,6 +29,24 @@ public class SwaglabTest extends BaseTest{
     public void login() {
         loginPage.inputUsername("standard_user","secret_sauce");
         Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/inventory.html");
+    }
+    @Test
+
+    public void buyProduct()
+    {
+        loginPage.inputUsername("standard_user","secret_sauce");
+        inventoryPage.addBackpack();
+        inventoryPage.clickOnCart();
+        Assert.assertEquals(cartPage.getInfoPrice(),"$29.99");
+        Assert.assertEquals(cartPage.getProductName(),"Sauce Labs Backpack");
+
+        cartPage.clickCheckout();
+        checkoutStepOnePage.inputPersonalInfo("Milos", "Boskovic", "11070");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/checkout-step-two.html");
+        checkoutStepOnePage.finish.click();
+        Assert.assertEquals(checkoutStepOnePage.getInfoMessage(),"Thank you for your order!");
+
+
     }
 @AfterMethod
 
